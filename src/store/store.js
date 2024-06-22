7810840c4021c83358c251b8e375b71657386a7a
SantiSanGal <santiagon.g.m@gmail.com>
2024-06-21 20:26:12 -0400

    utilización de RTK de Redux Toolkit

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { counterSlice } from './slices/counter'
 import { pokemonSlice } from './slices/pokemon'
+import { todosApi } from './apis/todosApi'
 
 export const store = configureStore({
     reducer: {
         counter: counterSlice.reducer,
-        pokemons: pokemonSlice.reducer
+        pokemons: pokemonSlice.reducer,
+        [todosApi.reducerPath]: todosApi.reducer,
     },
+    middleware: (getDefualtMiddleware) => getDefualtMiddleware().concat(todosApi.middleware)
 })
